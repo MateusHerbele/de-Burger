9ec8fc4c3bd57fd1d2d10d0d27fbc00d1fbed662
Mateus Herbele <73367371+MateusHerbele@users.noreply.github.com>
2023-02-17 20:12:26 -0300

    Update dbheader.h

--- dbheader.h
+++ dbheader.h
@@ -3,14 +3,69 @@
 
 #include<stdio.h>  
 #include<stdlib.h>
+#include<time.h>
 
-struct client* createClient(int id, struct order order);
-void addClient(struct list* list, struct client* client);
-void removeClient(struct list* list, struct client* client);
-void sortTime(struct client client);
+// Estrutras dos pedidos ======================================================
+struct order{
+    int numClient;
+    int numIng;
+    int* ingredients;
+    int deadline;// 30 segundos por pedido, pode ser alterado
+    int points;// 50 pontos por pedido entregue, pode ser alterado
+};
+
+struct orderNode{
+    struct order* id;
+    struct orderNode* nxtOrder;
+};
+
+struct list{
+    struct orderNode* head;
+    struct orderNode* tail;
+    int size;
+};
+
+// =============================================================================
+// Pedido
+struct list* createList();
+
+struct orderNode* createNode(int id, struct order order);
+
+void addOrderNode(struct list* list, struct orderNode* orderNode);
+
+void removeOrderNode(struct list* list, struct orderNode* orderNode);
+
+void sortTime(struct orderNode* head);
+
 int genNumIng(int numIng);
 
+int genIngredients(int numIng);
 
+struct order* createOrder(int numIng, int* ingredients);
+
+//Estutura de dados da montagem=================================================
+struct local{
+    int x;
+    int y;
+};
+
+struct recipe{
+    int amountIng;
+    int ingredients[8];
+};
+
+struct player{
+    struct local local;
+    struct recipe recipe;
+};
+//=============================================================================
+
+//Montagem
+void kitchenPlaces(struct player* player);
+
+void addIngredient(struct player* player, int ingredient);
+
+void clearRecipe(int* recipe);
 
 
 #endif // DEBURGUER_H
